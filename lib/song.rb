b030538bde9897db52254d4e895daf85328ba6b9
Chantel Lowe <chantellowe17@gmail.com>
2019-09-14 13:14:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,9 +26,9 @@ class Song
 
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
+    artistname = file.split(" - ")[0]
     song = self.new(song_name)
-    song.artist_name = artist 
+    song.artist_name = artistname 
     song
   end
 
