b664c76dd47ee32c390a3321c421d6dae4159634
Chantel Lowe <chantellowe17@gmail.com>
2019-09-14 12:25:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,12 +19,8 @@ class Song
     artist.add_song(self) unless artist.songs.include?(self)
   end
 
-  def self.new_by_filename(filename)
-    artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
-    song = self.new(song_name)
-    song.artist = Artist.find_or_create_by_name(artist_name)
-    song.genre = genre_name
-    song.save
+  def self.new_by_filename(file)
+    
   end
 
 end
